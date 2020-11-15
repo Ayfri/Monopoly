176399c121f979979d0c931e22a5b690505371bf
Ayfri <pierre.ayfri@gmail.com>
2020-11-15 16:04:20 +0100

    Renamed all .js output files to .mjs with a task.

--- gulpFile.js
+++ gulpFile.js
@@ -1,10 +1,20 @@
 const gulp = require('gulp');
+const clean = require("gulp-clean")
 const tsc = require('gulp-typescript');
+const rename = require('gulp-rename');
 const tsProject = tsc.createProject('tsconfig.json');
 
 function build() {
-	return gulp.src('src/**/*.ts').pipe(tsProject()).pipe(gulp.dest('dist'));
+	return gulp.src('src/**/*.ts')
+	           .pipe(tsProject())
+	           .pipe(rename(path => path.extname = '.mjs'))
+	           .pipe(gulp.dest('dist'))
 }
 
-exports.build = build;
+function cleanup() {
+	return gulp.src('dist/**/*.', {read: false})
+		.pipe(clean({force:true}))
+}
+
+exports.build = gulp.series([cleanup, build]);
 
